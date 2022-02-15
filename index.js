cdcf0c534e98bba1450c911cbd6735850d5b4b1f
insr531 <insoorhee@hotmail.com>
2022-02-15 13:50:25 -0800

    rm proxy in package json

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
